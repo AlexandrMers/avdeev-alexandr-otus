b401fd405191bb770cabd9a4a14e0aee2c21d11d
Алекандр Авдеев <alexander.avdeev@balance-pl.ru>
2022-01-31 08:59:57 +0300

    fix sorting small files

--- node-2/index.ts
+++ node-2/index.ts
@@ -4,9 +4,11 @@ import { sortSmallFiles } from './utils/sortSmallFiles'
 
 const processRun = async () => {
   try {
-    await createWriteStreamFunction('./fileExample.txt')
-    const filesDir = await splitBigFileToSmallerFiles('./fileExample.txt', 15)
+    await createWriteStreamFunction('./bigFile100mb.txt')
+    const filesDir = await splitBigFileToSmallerFiles('./bigFile100mb.txt', 20)
+    console.log('\n сортировка файлов ...')
     const sortedFiles = await sortSmallFiles(filesDir)
+    console.log('\n сортировка прошла успешно ...')
     console.log('sortedFiles -> ', sortedFiles)
   } catch (error) {
     console.error('error here ->', error)
--- node-2/utils/sortSmallFiles.ts
+++ node-2/utils/sortSmallFiles.ts
@@ -1,26 +1,24 @@
 import fs from 'fs'
-import path from 'path'
-import { sortMergeFunc } from './sortWithMerging'
 import { createPathToFile } from './createPathToFile'
 
 export const sortSmallFiles = async (directories: string[]) => {
-  for await (const fileResult of asyncPromiseGenerator(directories)) {
-    console.log('-----iteration-----')
-    console.log(fileResult)
-    console.log('-----iteration-----')
-  }
+  return sortSeparatedFiles(directories)
 }
 
-async function * asyncPromiseGenerator (directories: string[]) {
-  let count = 1
-  for (const url of directories) {
-    const pathToFile = createPathToFile('sortedFiles', count++)
+function sortSeparatedFiles (directories: string[]) {
+  return directories.map((dir, order) => {
+    const pathToFile = createPathToFile('sortedFiles', order + 1)
 
-    const sortedFile = await fs.promises.readFile(url, { encoding: 'utf-8' })
-    const arrayOfNumbers = sortedFile.split('\n')
-    const sortedNumbers = sortMergeFunc(arrayOfNumbers)
-    const joinedString = sortedNumbers.join('\n')
+    const data = fs.readFileSync(dir)
+      .toString()
+      .split('\n')
+      .map(Number)
+      .sort((a, b) => a - b)
+      .filter(num => !isNaN(num))
+      .join('\n')
 
-    yield await fs.promises.writeFile(path.resolve(pathToFile), joinedString, 'utf-8')
-  }
+    fs.writeFileSync(pathToFile, data)
+
+    return pathToFile
+  })
 }
